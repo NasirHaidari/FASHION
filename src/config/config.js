fce2994646cd4390da888061e8b7132928b98f2c
NasirHaidari <nasir.haidri@htomail.com>
2021-05-02 02:29:55 +0200

    created a react app and firebase

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_apikey,
+  authDomain: process.env.REACT_APP_authDomin,
+  projectId: process.env.REACT_APP_projectID,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
